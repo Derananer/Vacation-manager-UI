3b16252e03ddc0d0f86c07f11c80d37697459121
Derananer <10ilay1998@gmail.com>
2019-07-15 02:01:47 +0400

    start employee service

--- src/app/employees.service.ts
+++ src/app/employees.service.ts
@@ -1,25 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Employee } from './employees/employee';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
+
 	//here we need list of emps
 	//(we can create list that would update after ever action)
+  department = "";
+  employeeServiceURL = "http://localhost:8079/services/employee-service/";
+  employeesURL = "employees";
+  employeeURL = "employee";
+  addEmployeeURL = "add-employeeURL";
+
+  standratHeaders = new HttpHeaders({
+    'department': this.department, 
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  });
 
   getEmployees():Observable<Employee[]>{
-  	//here we http request
+    this.http.get<Employee[]>(this.employeeServiceURL + this.employeesURL, {"headers": this.standratHeaders});
   	return ;
   }
   addEmpoloyee():Observable<Employee>{
 
   	return;
   }
-  deletEmployee():Observable<>{
 
-  }
+  /*deleteEmployee():Observable<>{
 
-  constructor() { }
+  }*/
+
+ constructor(private http: HttpClient) { }
 }
